48b54405ee1ec1471787293e791cb3ae21a773a3
David Tran <davidlamt@users.noreply.github.com>
2021-07-07 21:33:05 -0700

    Remove Reddit logo, recommended posts, and promotions

--- removeRedditDistractions.js
+++ removeRedditDistractions.js
@@ -1,9 +1,22 @@
 function removeRedditDistractions() {
-  const elementSelectorsToRemove = ['._30BbATRhFv3V83DHNDjJAO', '._19oWd7e3z7-ztUGzdIoCR7'];
+  const elementSelectorsToRemove = [
+    'a[aria-label="Home"]', // Logo
+    '._19oWd7e3z7-ztUGzdIoCR7', // Login buttons home
+    '._1JBkpB_FOZMZ7IPr3FyNfH' // Login buttons search
+  ];
 
   // Home page
   if (window.location.pathname === '/') {
-    elementSelectorsToRemove.push(...['._3ozFtOe6WpJEMUtxDOIvtU']);
+    elementSelectorsToRemove.push(...['._3ozFtOe6WpJEMUtxDOIvtU']); // Home page content
+  }
+
+  // Subreddit community page / post
+  if (window.location.pathname.startsWith('/r/')) {
+    elementSelectorsToRemove.push(
+      ...[
+        '._2l7c_Oz0UVsamALvPrlznq', // Recommended posts 
+        '.promotedlink', // Promotions
+      ]); 
   }
 
   console.log('Reddit distraction selectors:', elementSelectorsToRemove);
