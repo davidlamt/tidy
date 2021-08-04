c4b3bd854ebc8b38083ff86bc24d51711ce6cf62
David Tran <davidlamt@users.noreply.github.com>
2021-08-03 20:21:09 -0700

    Add TypeScript

--- src/background.ts
+++ src/background.ts
@@ -1,19 +1,19 @@
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'loading' || changeInfo.status === 'complete') {
-    if (tab.url.includes('imdb.com')) {
+    if (tab?.url?.includes('imdb.com')) {
       console.log(`IMDb detected. Executing removal script when status is ${changeInfo.status}...`);
       browser.tabs.executeScript(tabId, {
         file: '/removeIMDbDistractions.js',
         runAt: 'document_start',
       });
     }
-    else if (tab.url.includes('reddit.com')) {
+    else if (tab?.url?.includes('reddit.com')) {
       console.log(`Reddit detected. Executing removal script when status is ${changeInfo.status}...`);
       browser.tabs.executeScript(tabId, {
         file: '/removeRedditDistractions.js',
         runAt: 'document_start',
       });
-    } else if (tab.url.includes('youtube.com')) {
+    } else if (tab?.url?.includes('youtube.com')) {
       console.log(`YouTube detected. Executing removal script when status is ${changeInfo.status}...`);
       browser.tabs.executeScript(tabId, {
         file: '/removeYouTubeDistractions.js',
